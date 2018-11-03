1075fa3523d8685985816c50151102cc82c7b636
CaloRen <joelcalo02@gmail.com>
2018-11-02 18:03:35 -0700

    Added methods in the MLinkedList class

--- src/MLinkedList.java
+++ src/MLinkedList.java
@@ -15,6 +15,50 @@ public class MLinkedList<T> {
 			this(value, null, null);
 		}
 		
+		Node first = null;
+		Node last = null;
+		
+		
+		//CHECKS IF THE LINKEDLIST IS EMPTY
+		
+		public boolean isEmpty() {
+			return first == null;
+		}
+		
+		
+		//CHECKS THE SIZE OF THE LINKEDLIST
+		
+		public int checkListSize() {
+			int count = 0;
+			Node tempNode = first;
+			
+			while(tempNode != null) {
+				count++;
+				tempNode = tempNode.next;
+			}
+			return count;
+		}
+		
+		
+		//ADDNODE METHODS
+		
+		public void addNode(int index, T val) {
+			if(index < 0 || index > checkListSize()) {
+				System.out.println("Index " + index + " is out of bounds");
+			}
+			
+			if(index == 0)
+			{
+				first = new Node(val);
+				if(last == null) {
+				last = first;
+				}
+			}
+		}
+		
+		public void addNode(T val) {
+			
+		}
 	}
 }
 
