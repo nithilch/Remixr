e952f6adb6b7679fa2f99b0e6acdffb7021cf9b0
Nithil <nithilaksha@gmail.com>
2018-11-02 17:48:20 -0700

    Created MLinkedList with Node class

--- src/MLinkedList.java
+++ src/MLinkedList.java
@@ -1,4 +1,20 @@
 
-public class MLinkedList {
-
+public class MLinkedList<T> {
+	
+	private class Node<T>{
+		T value;
+		Node prev, next;
+		
+		public Node(T value, Node prev, Node next) {
+			this.value = value;
+			this.prev = prev;
+			this.next = next;
+		}
+		
+		public Node(T value) {
+			this(value, null, null);
+		}
+		
+	}
 }
+
