0d510b364abbb9d6023890bb88d0fd9191c6befb
Nithil <nithilaksha@gmail.com>
2018-11-04 00:41:53 -0700

    Added new MLinkedList Methods to add element to first and last

--- src/MLinkedList.java
+++ src/MLinkedList.java
@@ -1,64 +1,68 @@
 
 public class MLinkedList<T> {
-	
-	private class Node<T>{
+
+	// NODE CLASS AND CONSTRUCTOR
+	class Node {
 		T value;
 		Node prev, next;
-		
-		public Node(T value, Node prev, Node next) {
+
+		Node(T value, Node prev, Node next) {
 			this.value = value;
 			this.prev = prev;
 			this.next = next;
 		}
-		
-		public Node(T value) {
-			this(value, null, null);
+
+		Node(T val) {
+			this(val, null, null);
 		}
-		
-		Node first = null;
-		Node last = null;
-		
-		
-		//CHECKS IF THE LINKEDLIST IS EMPTY
-		
-		public boolean isEmpty() {
-			return first == null;
+
+	}
+
+	Node first, last;
+
+	public MLinkedList() {
+		first = null;
+		last = null;
+	}
+	// CHECKS IF THE LINKEDLIST IS EMPTY
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	// CHECKS THE SIZE OF THE LINKEDLIST
+
+	public int size() {
+		int count = 0;
+		Node tempNode = first;
+
+		while (tempNode != null) {
+			count++;
+			tempNode = tempNode.next;
 		}
-		
-		
-		//CHECKS THE SIZE OF THE LINKEDLIST
-		
-		public int checkListSize() {
-			int count = 0;
-			Node tempNode = first;
-			
-			while(tempNode != null) {
-				count++;
-				tempNode = tempNode.next;
-			}
-			return count;
+		return count;
+	}
+
+	public void addFirst(T val) {
+		Node temp = new Node(val, null, first);
+		if (first != null) {
+			first.prev = temp;
 		}
-		
-		
-		//ADDNODE METHODS
-		
-		public void addNode(int index, T val) {
-			if(index < 0 || index > checkListSize()) {
-				System.out.println("Index " + index + " is out of bounds");
-			}
-			
-			if(index == 0)
-			{
-				first = new Node(val);
-				if(last == null) {
-				last = first;
-				}
-			}
-		}
-		
-		public void addNode(T val) {
-			
+		first = temp;
+		if (last == null) {
+			last = temp;
 		}
 	}
-}
 
+	public void addLast(T val) {
+		Node temp = new Node(val, last, null);
+		if (last != null) {
+			last.next = temp;
+		}
+		last = temp;
+		if (first == null) {
+			first = temp;
+		}
+	}
+
+}
